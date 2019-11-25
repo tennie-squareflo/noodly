e74d98d0b9f97333f0783c3b6e53741990731174
ever-dev <everdev0923@gmail.com>
2019-11-25 14:59:09 -0500

    test: test env variables

--- core/routes.php
+++ core/routes.php
@@ -7,15 +7,19 @@ $host_url = $_SERVER['HTTP_HOST'];
 $request_uri = $_SERVER['REQUEST_URI'];
 
 $current_role = '';
+$publisher_domain = '';
+
 if (ENV === 'production') {
   $current_role = $host_url === 'noodly.io' || $host_url === 'www.noodly.io' ? 'admin' : 'publisher';
 } else {
   $current_role = substr($request_uri, 0, 6) === '/admin' ? 'admin' : 'publisher';
-  $request_uri = substr($request_uri, strlen($current_role));
+  $publisher_domain = strstr(substr($request_uri, 1), '/', true);
+  $request_uri = strstr(substr($request_uri, 1), '/');
 }
 
 echo "Current Role: $current_role<br/>";
 echo "Request URI: $request_uri<br/>";
+echo "Publisher domain: $publisher_domain<br/>";
 
 
 ?>

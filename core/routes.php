0475ad6dd554c9069b627ae3dee1832859d12f75
ever-dev <everdev0923@gmail.com>
2019-11-25 14:44:38 -0500

    test: test env variables

--- core/routes.php
+++ core/routes.php
@@ -3,7 +3,19 @@ foreach ($_SERVER as $key => $value) {
   echo "$key $value<br/>";
 }
 
-//$current_role = ;
+$host_url = $_SERVER['HTTP_HOST'];
+$request_uri = $_SERVER['REQUEST_URI'];
+
+$current_role = '';
+if (ENV === 'production') {
+  $current_role = $host_url === 'noodly.io' || $host_url === 'www.noodly.io' ? 'admin' : 'publisher';
+} else {
+  $current_role = substr($request_uri, 0, 6) === '/admin' ? 'admin' : 'publisher';
+  $request_uri = substr($request_uri, strlen($current_role));
+}
+
+echo "Current Role: $current_role<br/>";
+echo "Request URI: $request_uri<br/>";
 
 
 ?>

cb8c016e60fdb95a911a718428cf1bd367e822cb
ever-dev <everdev0923@gmail.com>
2019-11-28 12:02:30 -0500

    feat: implement domain validation for php and javascript

--- assets/custom/admin/publisher-register/register.js
+++ assets/custom/admin/publisher-register/register.js
@@ -1,8 +1,15 @@
-
 "use strict";
 
 import("../../../vendors/custom/slim/slim.kickstart.min.js");
 $(function() {
+  jQuery.validator.addMethod(
+    "checkDomain",
+    function(value) {
+      return value.match(/[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9]/g)[0] === value;
+    },
+    "Domain must be greater than zero"
+  );
+
   $("#register-form").validate({
     rules: {
       name: {
@@ -10,9 +17,10 @@ $(function() {
       },
       // logo: {
       //   required: true
-      // },  
+      // },
       domain: {
-        required: true
+        required: true,
+        checkDomain: true
       },
       phone: {
         required: true
@@ -38,78 +46,79 @@ $(function() {
       }
     },
     messages: {
-      email: "Please enter a valid email address"
+      email: "Please enter a valid email address",
+      checkDomain: "Please enter a valid domain"
     },
     submitHandler: function(form) {
       $.ajax({
-        url: BASE_URL + 'publishers/action/edit',
+        url: BASE_URL + "publishers/action/edit",
         data: $(form).serialize(),
-        dataType: 'json',
-        method: 'POST',
+        dataType: "json",
+        method: "POST",
         success: function(res) {
           if (res.code == 0) {
             //location.href = 'dashboard.php';
             toastr.options = {
-              "closeButton": false,
-              "debug": false,
-              "newestOnTop": false,
-              "progressBar": false,
-              "positionClass": "toast-top-right",
-              "preventDuplicates": false,
-              "onclick": null,
-              "showDuration": "300",
-              "hideDuration": "1000",
-              "timeOut": "5000",
-              "extendedTimeOut": "1000",
-              "showEasing": "swing",
-              "hideEasing": "linear",
-              "showMethod": "fadeIn",
-              "hideMethod": "fadeOut"
+              closeButton: false,
+              debug: false,
+              newestOnTop: false,
+              progressBar: false,
+              positionClass: "toast-top-right",
+              preventDuplicates: false,
+              onclick: null,
+              showDuration: "300",
+              hideDuration: "1000",
+              timeOut: "5000",
+              extendedTimeOut: "1000",
+              showEasing: "swing",
+              hideEasing: "linear",
+              showMethod: "fadeIn",
+              hideMethod: "fadeOut"
             };
-            
+
             toastr.success(res.message);
             setTimeout(() => {
               location.href = BASE_URL + `publishers/edit/${res.id}`;
             }, 3000);
           } else {
             toastr.options = {
-              "closeButton": false,
-              "debug": false,
-              "newestOnTop": false,
-              "progressBar": false,
-              "positionClass": "toast-top-right",
-              "preventDuplicates": false,
-              "onclick": null,
-              "showDuration": "300",
-              "hideDuration": "1000",
-              "timeOut": "5000",
-              "extendedTimeOut": "1000",
-              "showEasing": "swing",
-              "hideEasing": "linear",
-              "showMethod": "fadeIn",
-              "hideMethod": "fadeOut"
+              closeButton: false,
+              debug: false,
+              newestOnTop: false,
+              progressBar: false,
+              positionClass: "toast-top-right",
+              preventDuplicates: false,
+              onclick: null,
+              showDuration: "300",
+              hideDuration: "1000",
+              timeOut: "5000",
+              extendedTimeOut: "1000",
+              showEasing: "swing",
+              hideEasing: "linear",
+              showMethod: "fadeIn",
+              hideMethod: "fadeOut"
             };
-            
+
             toastr.warning(res.message);
           }
-        }, 
+        },
         error: function(res) {
           toastr.options = {
-            "closeButton": false,
-            "debug": false,
-            "newestOnTop": false,
-            "progressBar": false,
-            "positionClass": "toast-top-right",
-            "preventDuplicates": false,
-            "onclick": null,
-            "showDuration": "300",
-            "hideDuration": "1000",
-            "timeOut": "5000",
-            "extendedTimeOut": "1000",
-            "showEasing": "swing",
-            "hideEasing": "linear",
-            "showMethod": "fadeIn",
-            "hideMethod": "fadeOut"
+            closeButton: false,
+            debug: false,
+            newestOnTop: false,
+            progressBar: false,
+            positionClass: "toast-top-right",
+            preventDuplicates: false,
+            onclick: null,
+            showDuration: "300",
+            hideDuration: "1000",
+            timeOut: "5000",
+            extendedTimeOut: "1000",
+            showEasing: "swing",
+            hideEasing: "linear",
+            showMethod: "fadeIn",
+            hideMethod: "fadeOut"
           };
           toastr.error(res.responseJSON.message);
         }
@@ -117,10 +126,16 @@ $(function() {
     }
   });
 
-  $('#save-btn').click(() => {
-    $('#register-form').submit();
+  $("#save-btn").click(() => {
+    $("#register-form").submit();
   });
   setTimeout(() => {
-    $('#register-form').find('input[name="logo"]').val($('#register-form').find('input[type="file"]').attr('data-value'));
+    $("#register-form")
+      .find('input[name="logo"]')
+      .val(
+        $("#register-form")
+          .find('input[type="file"]')
+          .attr("data-value")
+      );
   }, 500);
-}); 
+});

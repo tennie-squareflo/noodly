17ed241a2fcac44058a3607b2167ed5ded61e100
ever-dev <everdev0923@gmail.com>
2019-12-17 11:23:32 -0500

    fix: fix avatar upload on publisher

--- noodly-publisher/controllers/api.php
+++ noodly-publisher/controllers/api.php
@@ -1,11 +1,25 @@
 <?php
-require_once(PUBLISHER_PATH.'core/auth_controller.php');
 
-class Api_Controller extends Auth_Controller {
+class Api_Controller extends Core_Controller {
   function __construct() {
-    parent::__construct(true, true);
+    parent::__construct('admin');
   }
 
-  function index() {
+  public function avatar_upload() {
+    $this->load_library('slim_image_uploader');
+    $this->slim_image_uploader->image_upload('avatar', ASSETS_PATH.'media/avatars/');
   }
-} 
+
+  public function check_email() {
+    $post = $_POST;
+    $this->load_model('user');
+
+    // check if email exists;
+    $user = $this->user_model->get_one(array('email' => $post['email']));
+    if (count($user)) {
+      $this->response(array('code' => 0, 'name' => $user['firstname']));
+    } else {
+      $this->response(array('code' => 1));
+    }
+  }
+}

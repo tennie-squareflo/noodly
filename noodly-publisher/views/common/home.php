8fcaa7fa965fcb275ebe4f2a782c46744bc7df1c
ever-dev <everdev0923@gmail.com>
2020-01-16 16:45:42 -0500

    feat: implement the publisher homepage

--- noodly-publisher/views/common/home.php
+++ noodly-publisher/views/common/home.php
@@ -1,5 +1,9 @@
 <?php 
-
+  $this->load_model('category');
+  $this->load_model('story');
+  $this->load_model('publisher');
+  $categories = $this->category_model->get_categories($publisher['pid'], 0);
+  $stories = $this->view_data['stories'] = $this->story_model->get_recent_stories($publisher['pid'], 0);
 ?>
 <section class="posts blog-masonry">
   <div class="container">
@@ -8,12 +12,12 @@
         foreach ($stories as $key => $story) {
       ?>
         <div class="post-block post-classic">
-          <div class="post-img"><img src="https://images.pexels.com/photos/3133685/pexels-photo-3133685.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="post image"></div>
+          <div class="post-img"><img src="<?php echo ASSETS_URL;?>media/stories/<?php echo $story['thumb_image'];?>" alt="post image"></div>
           <div class="post-detail">
             <div class="post-credit">
-              <h5 class="upload-day">February 27, 2019</h5>
-              <div class="post-tag"><a href="index.html">section</a></div>
-            </div><a class="post-title regular" href="story-detail.html"><?php echo $story['title'];?></a>
+              <h5 class="upload-day"><?php echo $story['created_at'] ?></h5>
+              <div class="post-tag"><a href="index.html"><?php echo $categories[$story['cid']]['name']; ?></a></div>
+            </div><a class="post-title regular" target="_blank" href="<?php echo BASE_URL.'story/preview/'.$story['url'];?>"><?php echo $story['title'];?></a>
           </div>
         </div>
       <? } ?>
--- noodly-publisher/views/common/layout/header.php
+++ noodly-publisher/views/common/layout/header.php
@@ -1,8 +1,7 @@
 <?php
   $this->load_model('category');
-  $this->load_model('story');
-  $this->load_model('user');
   $this->load_model('publisher');
+  $categories = $this->category_model->get_categories($publisher['pid'], 0);
 ?>
 <body>
     <div id="main">
@@ -11,18 +10,18 @@
           <div class="container">
             <div class="header-menu">
               <div class="row no-gutters align-items-center justify-content-center">
-                <div class="col-4 col-md-2"><a class="logo" href="index.html"><img src="<?php echo ASSETS_URL;?>media/logos/<?php echo $publisher['logo'];?>" alt="logo" width="170px"></a></div>
+                <div class="col-4 col-md-2"><a class="logo" href="<?php echo BASE_URL; ?>"><img src="<?php echo ASSETS_URL;?>media/logos/<?php echo $publisher['logo'];?>" alt="logo" width="170px"></a></div>
                 <div class="col-8 col-md-8">
                   <div class="mobile-menu"><a href="#" id="showMenu"><i class="fas fa-bars"></i></a></div>
                   <nav class="navigation">
                     <ul>
 
-                      <li class="nav-item"><a class="pisen-nav-link active" href="index.html">Latest</a></li>
+                      <li class="nav-item"><a class="pisen-nav-link active" href="<?php echo BASE_URL; ?>">Latest</a></li>
                       <li class="nav-item"><a class="pisen-nav-link" href="popular.html">Popular</a></li>
                       <li class="nav-item"><a class="pisen-nav-link" href="#">Sections</a><i class="submenu-opener fas fa-plus"></i>
                         <ul class="sub-menu">
                           <?php
-                            $categories = $this->category_model->get_categories($publisher['pid'], 0);
+                            
                             foreach ($categories as $key => $category) {
                           ?>
                             <li class="sub-menu_item"><a class="sub-menu-link" href="#"><?php echo $category['name']; ?></a></li>

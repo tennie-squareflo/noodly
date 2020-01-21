a53b1d5a767573ade181ec3db4f870a8ebf9ddb8
ever-dev <everdev0923@gmail.com>
2020-01-21 06:01:43 -0500

    fix: fix contact page

--- noodly-publisher/views/common/contact.php
+++ noodly-publisher/views/common/contact.php
@@ -1,17 +1,21 @@
-
+<style>
+  #main {
+    padding-top: 150px;
+  }
+</style>
       <section class="contact">
           <div class="container">
             <div class="row">
               <div class="col-12">
                 <div class="map-contact">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29794.31661118451!2d105.82941046411365!3d21.02109628378552!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab9926e7bd67%3A0x580e078874d5df1e!2zVsSDbiBNaeG6v3UtIFF14buRYyBU4butIEdpw6Ft!5e0!3m2!1svi!2s!4v1556253249354!5m2!1svi!2s" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+                  <iframe src="https://www.google.com/maps/place/1814+westport+rd,+kansas+city" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
                 </div>
               </div>
               
               <div class="col-12">
                 <div class="leave-message text-center">
-                  <h1>Leave Message</h1>
-                  <p>Our staff will call back later and answer your questions</p>
+                  <h1>Contact Us</h1>
+                  <p>Fill out the form below to get in touch with us</p>
                   <form>
                     <div class="form-row">
                       <div class="form-group col-md-6">

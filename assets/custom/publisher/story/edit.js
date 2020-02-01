dfa7cc0a9835bc8e430ac663c98a9d2694a5754d
ever-dev <everdev0923@gmail.com>
2020-02-01 17:35:50 -0500

    show modal to complete fields

--- assets/custom/publisher/story/edit.js
+++ assets/custom/publisher/story/edit.js
@@ -246,6 +246,26 @@ $(function() {
   $('#direct-draft-btn').click((e) => {
     if (isStoryFormValid()) {
       $('#add-client-modal').modal();
+    } else {
+      toastr.options = {
+        closeButton: false,
+        debug: false,
+        newestOnTop: false,
+        progressBar: false,
+        positionClass: "toast-top-right",
+        preventDuplicates: false,
+        onclick: null,
+        showDuration: "300",
+        hideDuration: "1000",
+        timeOut: "5000",
+        extendedTimeOut: "1000",
+        showEasing: "swing",
+        hideEasing: "linear",
+        showMethod: "fadeIn",
+        hideMethod: "fadeOut"
+      };
+      toastr.error(`Complete the fields below`);
+      return;
     }
   })
 
